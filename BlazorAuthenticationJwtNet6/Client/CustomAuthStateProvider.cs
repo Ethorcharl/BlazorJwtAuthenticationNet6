8f18c6eccf3c9d4b9468a6a158fa86e8072939b1
Ethorcharl <zenja_riga5@yahoo.com>
2023-06-01 18:28:32 +0300

    Login page and login/logout

--- BlazorAuthenticationJwtNet6/Client/CustomAuthStateProvider.cs
+++ BlazorAuthenticationJwtNet6/Client/CustomAuthStateProvider.cs
@@ -1,19 +1,40 @@
-﻿using System.Security.Claims;
+﻿using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text.Json;
 
 namespace BlazorAuthenticationJwtNet6.Client
 {
     public class CustomAuthStateProvider : AuthenticationStateProvider
     {
+        private readonly ILocalStorageService _localStorage;
+        private readonly HttpClient _http;
+
+        public CustomAuthStateProvider(ILocalStorageService localStorage, HttpClient http)
+        {
+            _localStorage = localStorage;
+            _http = http;
+        }
+
+
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
 
-        //idk but for jwt need next fields : {"http:///schemas.xmlsoap.org/ws/2005/05/identity/claims/name": "Test", "http:///schemas.microsoft.com/ws/2008/06/identity/claims/role": "Test"
-        //thi is correct : eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlRlc3QifQ.nJ_-Dt8fG-SGlu5OvAAJn3Z30Yguq_Y6yhznNvECsaTuP3JAC0FRUotbm2ZIx8YxMtfpDFagDFs125-Ul7QGpA
-        // //eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJUZXN0In0.yTEuL9xUfTN_g4WNicC6TP2UvgXx2f7Hcv1Skp2Z9krhROl2FVkyzGSZGb5V2SgBd2VY3fTy2ooWl9cJOCFBjw
-        string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlRlc3QifQ.nJ_-Dt8fG-SGlu5OvAAJn3Z30Yguq_Y6yhznNvECsaTuP3JAC0FRUotbm2ZIx8YxMtfpDFagDFs125-Ul7QGpA";
-            //var identity = new ClaimsIdentity(); // user not auth if bracket empty 
-            var identity = new ClaimsIdentity(ParseClaimsFromJwt(token),"jwt"); 
+            //idk but for jwt need next fields : {"http:///schemas.xmlsoap.org/ws/2005/05/identity/claims/name": "Test", "http:///schemas.microsoft.com/ws/2008/06/identity/claims/role": "Test"
+            //thi is correct : eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlRlc3QifQ.nJ_-Dt8fG-SGlu5OvAAJn3Z30Yguq_Y6yhznNvECsaTuP3JAC0FRUotbm2ZIx8YxMtfpDFagDFs125-Ul7QGpA
+            // //eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJUZXN0In0.yTEuL9xUfTN_g4WNicC6TP2UvgXx2f7Hcv1Skp2Z9krhROl2FVkyzGSZGb5V2SgBd2VY3fTy2ooWl9cJOCFBjw
+            //string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlRlc3QifQ.nJ_-Dt8fG-SGlu5OvAAJn3Z30Yguq_Y6yhznNvECsaTuP3JAC0FRUotbm2ZIx8YxMtfpDFagDFs125-Ul7QGpA";
+            string token = await _localStorage.GetItemAsStringAsync("token");
+
+            var identity = new ClaimsIdentity(); // user not auth if bracket empty 
+            _http.DefaultRequestHeaders.Authorization = null;
+
+            if (!string.IsNullOrEmpty(token))
+
+            {
+                identity = new ClaimsIdentity(ParseClaimsFromJwt(token), "jwt");
+                _http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token.Replace("\"",""));
+            }
+
             var user = new ClaimsPrincipal(identity);
             var state = new AuthenticationState(user);
             NotifyAuthenticationStateChanged(Task.FromResult(state));
---
+++ BlazorAuthenticationJwtNet6/Server/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using BlazorAuthenticationJwtNet6.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAuthenticationJwtNet6.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<string>> Login(UserLoginDto request) 
+        {
+            string token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1lIjoiVGVzdCIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6IlRlc3QifQ.nJ_-Dt8fG-SGlu5OvAAJn3Z30Yguq_Y6yhznNvECsaTuP3JAC0FRUotbm2ZIx8YxMtfpDFagDFs125-Ul7QGpA";
+            return token;
+
+        }
+
+    }
+}
